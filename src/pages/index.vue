3ac038131c1bf9aa3cb384c5f4dfe3f4c6475eba
tmartty <tomasmartty@gmail.com>
2018-04-07 10:17:50 -0300

    initial commit - quasar setup

---
+++ src/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+	<q-page class="flex flex-center">
+		Motion control your music
+	</q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+	name: 'PageIndex'
+}
+</script>
